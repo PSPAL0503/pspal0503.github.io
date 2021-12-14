c3493cfab7b65f852b53270804c5868e78fb99bf
PSPAL0503 <pratikpal0531@gmail.com>
2021-12-13 19:56:24 -0500

    Commit

---
+++ java_script.js
@@ -0,0 +1,8 @@
+function dynamic() {
+    if (document.getElementById('hiring').checked) {
+        document.getElementById('hr').style.display = "block";
+    } else {
+        document.getElementById('hr').style.display = "none";
+    }
+    return;
+}
